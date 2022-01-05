f47170418e93233427b542e8604890a136f18490
Furkan Kaya <hello@iamfurkan.com>
2022-01-05 04:35:27 +0300

    no problemo!

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
 
 let resimler = [];
-let value=0;
+let value=10000000;
 let index = 0; 
 function preload() {
     for(var i=1;i<37;i++){
@@ -19,17 +19,14 @@ function setup() {
 function draw() {
     rectMode(CORNER)
     background("#FFC0CB"); //Arkaplanı renklendirdik. 
-    if (mouseIsPressed) {
-        //index = map(value, 0, width, 0, 35*4, true); // mouseX'ini en baştan en sona olan koordinatı en baş 0 en son 36 olacak şekilde güncelle
-    }
+
     index = int(value)%35; // float olamayacağı için int'e çevir
 
-    console.log(movedX, value, index);
-
-    image(resimler[index], width/2-400, 0-25); //resmi ekrana bas
+    if(resimler[index]!=null)
+        image(resimler[index], width/2-400, 0-25); //resmi ekrana bas
 }
 
 function mouseDragged() {
-    
-    value = value + movedX/10;
+    if(value<=1) value=10000000;
+    value = value + int(movedX/10);   
 }
