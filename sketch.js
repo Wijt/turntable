c2cdc5966c3ccbe4e4849baa90ea0b83fefd1975
Ayşenur Erdoğan <aysenurerdoganx@gmail.com>
2022-01-05 04:24:39 +0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 
 let resimler = [];
-
+let value=0;
+let index = 0; 
 function preload() {
     for(var i=1;i<37;i++){
         resimler.push(loadImage('360/A_'+zfill(i,3)+'.png'));
@@ -18,8 +19,17 @@ function setup() {
 function draw() {
     rectMode(CORNER)
     background("#FFC0CB"); //Arkaplanı renklendirdik. 
-    index = map(mouseX, 0, width, 0, 35*4, true); // mouseX'ini en baştan en sona olan koordinatı en baş 0 en son 36 olacak şekilde güncelle
-    index = int(index)%35; // float olamayacağı için int'e çevir
-    console.log(index, pmouseX);
+    if (mouseIsPressed) {
+        //index = map(value, 0, width, 0, 35*4, true); // mouseX'ini en baştan en sona olan koordinatı en baş 0 en son 36 olacak şekilde güncelle
+    }
+    index = int(value)%35; // float olamayacağı için int'e çevir
+
+    console.log(movedX, value, index);
+
     image(resimler[index], width/2-400, 0-25); //resmi ekrana bas
+}
+
+function mouseDragged() {
+    
+    value = value + movedX/10;
 }
